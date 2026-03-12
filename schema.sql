e472587c2e00e68640fe158eb507a894cc9d571b
Sairam Devarakonda <sairamdsr143@gmail.com>
2026-03-12 12:36:12 +0530

    fix schema safe tables

--- schema.sql
+++ schema.sql
@@ -1,17 +1,16 @@
-
-CREATE TABLE movies(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-title TEXT,
-poster TEXT,
-review TEXT,
-created_at TEXT
+CREATE TABLE IF NOT EXISTS movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    poster TEXT,
+    created_at TEXT
 );
 
-CREATE TABLE comments(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-movie_id INTEGER,
-rating INTEGER,
-comment TEXT,
-likes INTEGER,
-created_at TEXT
+CREATE TABLE IF NOT EXISTS comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_id INTEGER,
+    rating INTEGER,
+    comment TEXT,
+    likes INTEGER DEFAULT 0,
+    created_at TEXT,
+    FOREIGN KEY(movie_id) REFERENCES movies(id)
 );
